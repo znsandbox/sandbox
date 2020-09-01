7098b8244a2cc72dc23d7bd8e373a27fd085f640
Vitaliy <theyamshikov@yandex.ru>
2020-09-01 21:05:37 +0600

    upd

---
+++ src/Lang/Migrations/m190101_082800_create_language_table.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Migrations;
+
+use Illuminate\Database\Schema\Blueprint;
+use PhpLab\Eloquent\Migration\Base\BaseCreateTableMigration;
+use PhpLab\Eloquent\Migration\Enums\ForeignActionEnum;
+
+class m190101_082800_create_language_table extends BaseCreateTableMigration
+{
+
+    protected $tableName = 'language';
+    protected $tableComment = 'Язык';
+
+    public function tableSchema()
+    {
+        return function (Blueprint $table) {
+            $table->string('code')->comment('Код языка');
+            $table->string('title')->comment('Название');
+            $table->string('name')->comment('Системное имя');
+            $table->string('locale')->comment('Локаль');
+            $table->boolean('is_main')->comment('По умолчанию?');
+            $table->boolean('is_enabled')->comment('Включен?');
+
+            $table->unique('title');
+            $table->unique('name');
+            $table->unique('locale');
+            $table->primary('code');
+        };
+    }
+
+}
