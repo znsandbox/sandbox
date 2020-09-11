7cad031c57aafe91300183381c6d07c6bd99ab9c
Vitaliy <theyamshikov@yandex.ru>
2020-09-12 01:26:04 +0600

    upd

--- src/RestClient/Yii/Api/Module.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ZnSandbox\Sandbox\RestClient\Yii\Api;
-
-use yii\base\Module as YiiModule;
-
-class Module extends YiiModule {
-
-}
--- src/RestClient/Yii/Web/traits/WebCrudTrait.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace ZnSandbox\Sandbox\RestClient\Yii\Web\traits;
-
-trait WebCrudTrait
-{
-
-}
