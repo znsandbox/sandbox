f032540055dc8d9a7bd6a0a992448063b1719503
Vitaliy <theyamshikov@yandex.ru>
2021-05-10 18:55:09 +0600

    upd

--- src/Casbin/Domain/Interfaces/Services/ManagerServiceInterface.php
+++ src/Casbin/Domain/Interfaces/Services/ManagerServiceInterface.php
@@ -2,9 +2,39 @@
 
 namespace ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Services;
 
+use ZnCore\Base\Exceptions\ForbiddenException;
+
 interface ManagerServiceInterface
 {
 
+    /**
+     * Проверка полномочий
+     * @param array $roleNames Роли пользователя
+     * @param array $permissionNames Полномочия
+     * @throws ForbiddenException
+     */
+    public function checkAccess(array $roleNames, array $permissionNames): void;
 
+    /**
+     * Имеют ли полномочия указанные роли? (Да/Нет)
+     * @param array $roleNames
+     * @param array $permissionNames
+     * @return bool
+     */
+    public function isCanByRoleNames(array $roleNames, array $permissionNames): bool;
+
+    /**
+     * Получить вложенные роли и полномочия для роли
+     * @param string $roleName Имя роли
+     * @return array
+     */
+    public function allNestedRolesByRoleName(string $roleName): array;
+
+    /**
+     * Получить вложенные роли и полномочия для массива ролей
+     * @param array $roleNames Массив имен ролей
+     * @return array
+     */
+    public function allNestedRolesByRoleNames(array $roleNames): array;
 }
 
--- src/Casbin/Domain/Services/ManagerService.php
+++ src/Casbin/Domain/Services/ManagerService.php
@@ -2,13 +2,13 @@
 
 namespace ZnSandbox\Sandbox\Casbin\Domain\Services;
 
-use ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Repositories\InheritanceRepositoryInterface;
-use ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Repositories\ManagerRepositoryInterface;
-use ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Services\ManagerServiceInterface;
 use Casbin\ManagementEnforcer;
 use ZnCore\Base\Exceptions\ForbiddenException;
 use ZnCore\Domain\Base\BaseService;
 use ZnCore\Domain\Interfaces\Libs\EntityManagerInterface;
+use ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Repositories\InheritanceRepositoryInterface;
+use ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Repositories\ManagerRepositoryInterface;
+use ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Services\ManagerServiceInterface;
 
 class ManagerService extends BaseService implements ManagerServiceInterface
 {
@@ -16,13 +16,16 @@ class ManagerService extends BaseService implements ManagerServiceInterface
     /** @var ManagementEnforcer */
     private $enforcer;
 
-    public function __construct(EntityManagerInterface $entityManager, InheritanceRepositoryInterface $inheritanceRepository, ManagerRepositoryInterface $managerRepository)
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        ManagerRepositoryInterface $managerRepository
+    )
     {
         $this->setEntityManager($entityManager);
         $this->enforcer = $managerRepository->getEnforcer();
     }
 
-    public function checkAccess(array $roleNames, array $permissionNames)
+    public function checkAccess(array $roleNames, array $permissionNames): void
     {
         $isCan = $this->isCanByRoleNames($roleNames, $permissionNames);
         if (!$isCan) {
@@ -30,7 +33,14 @@ class ManagerService extends BaseService implements ManagerServiceInterface
         }
     }
 
-    public function allNestedRolesByRoleName(string $roleName)
+    public function isCanByRoleNames(array $roleNames, array $permissionNames): bool
+    {
+        $all = $this->allNestedRolesByRoleNames($roleNames);
+        $intersect = array_intersect($permissionNames, $all);
+        return !empty($intersect);
+    }
+
+    public function allNestedRolesByRoleName(string $roleName): array
     {
         return $this->enforcer->getImplicitRolesForUser($roleName);
     }
@@ -43,12 +53,5 @@ class ManagerService extends BaseService implements ManagerServiceInterface
             $all = array_merge($all, $nested);
         }
         return $all;
-    }
-
-    public function isCanByRoleNames(array $roleNames, array $permissionNames): bool
-    {
-        $all = $this->allNestedRolesByRoleNames($roleNames);
-        $intersect = array_intersect($permissionNames, $all);
-        return !empty($intersect);
     }
 }
