65d3635db2f6548d1479fea14656d5fa514002db
Vitaliy <theyamshikov@yandex.ru>
2021-05-19 15:40:13 +0600

    upd

--- src/Casbin/Domain/Services/ManagerService.php
+++ src/Casbin/Domain/Services/ManagerService.php
@@ -3,8 +3,10 @@
 namespace ZnSandbox\Sandbox\Casbin\Domain\Services;
 
 use Casbin\ManagementEnforcer;
+use ZnBundle\User\Domain\Interfaces\Services\AuthServiceInterface;
 use ZnCore\Base\Exceptions\ForbiddenException;
 use ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Repositories\ManagerRepositoryInterface;
+use ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Services\AssignmentServiceInterface;
 use ZnSandbox\Sandbox\Casbin\Domain\Interfaces\Services\ManagerServiceInterface;
 
 class ManagerService implements ManagerServiceInterface
@@ -12,10 +14,25 @@ class ManagerService implements ManagerServiceInterface
 
     /** @var ManagementEnforcer */
     private $enforcer;
+    private $authService;
+    private $assignmentService;
 
-    public function __construct(ManagerRepositoryInterface $managerRepository)
+    public function __construct(
+        ManagerRepositoryInterface $managerRepository,
+        AuthServiceInterface $authService,
+        AssignmentServiceInterface $assignmentService
+    )
     {
         $this->enforcer = $managerRepository->getEnforcer();
+        $this->authService = $authService;
+        $this->assignmentService = $assignmentService;
+    }
+
+    public function checkMyAccess(array $permissionNames): void
+    {
+        $identityEntity = $this->authService->getIdentity();
+        $roleNames = $this->assignmentService->getRolesByIdentityId($identityEntity->getId());
+        $this->checkAccess($roleNames, $permissionNames);
     }
 
     public function checkAccess(array $roleNames, array $permissionNames): void
