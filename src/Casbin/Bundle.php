37b5733058924482695548156c1a1e3dd4c63262
Vitaliy <theyamshikov@yandex.ru>
2021-06-26 16:27:48 +0600

    upd

--- src/Casbin/Bundle.php
+++ src/Casbin/Bundle.php
@@ -7,6 +7,15 @@ use ZnCore\Base\Libs\App\Base\BaseBundle;
 class Bundle extends BaseBundle
 {
 
+    public function symfonyRpc(): array
+    {
+        return [
+            __DIR__ . '/Rpc/config/assignment-routes.php',
+            __DIR__ . '/Rpc/config/permission-routes.php',
+            __DIR__ . '/Rpc/config/role-routes.php',
+        ];
+    }
+
     public function migration(): array
     {
         return [
