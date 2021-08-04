3df157b5f395741fad458829e914f4366354274f
Vitaliy <theyamshikov@yandex.ru>
2021-08-04 17:18:53 +0600

    upd

--- src/Layout/Domain/config/container.php
+++ src/Layout/Domain/config/container.php
@@ -3,7 +3,6 @@
 return [
     'singletons' => [
         'ZnSandbox\Sandbox\Layout\Domain\Interfaces\Services\MenuServiceInterface' => 'ZnSandbox\Sandbox\Layout\Domain\Services\MenuService',
-
         'ZnSandbox\Sandbox\Layout\Domain\Interfaces\Repositories\MenuRepositoryInterface' => 'ZnSandbox\Sandbox\Layout\Domain\Repositories\File\MenuRepository',
     ],
 ];
--- src/Symfony/Bundle.php
+++ src/Symfony/Bundle.php
@@ -7,6 +7,15 @@ use ZnCore\Base\Libs\App\Base\BaseBundle;
 class Bundle extends BaseBundle
 {
 
+    public function deps(): array
+    {
+        return [
+            new \ZnCore\Base\Bundle(['all']),
+            new \ZnCore\Base\Libs\I18Next\Bundle(['all']),
+            new \ZnCore\Base\Libs\App\Bundle(['all']),
+        ];
+    }
+    
     public function container(): array
     {
         return [
