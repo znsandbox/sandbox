e6c75cca1f4b0e9734741baa52c41030da3020bd
Vitaliy <theyamshikov@yandex.ru>
2021-08-08 15:41:06 +0600

    upd

--- config/namespace.php
+++ config/namespace.php
@@ -1,2 +1,17 @@
 <?php
 
+use Zn\Example\Plus;
+
+$namespaces = [
+    'Zn\\Example' => __DIR__ . '/../src/Example',
+];
+
+//dd(class_exists(\ZnCore\Base\Helpers\ComposerHelper::class));
+
+foreach ($namespaces as $namespace => $path) {
+    $path = realpath($path);
+//    dd(\ZnCore\Base\Legacy\Yii\Helpers\FileHelper::scanDir($path));
+    //dd($namespace, $path);
+    //\ZnCore\Base\Helpers\ComposerHelper::register($namespace, $path);
+    //dd(Plus::run(1, 6));
+}
---
+++ src/Example/Plus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zn\Example;
+
+class Plus
+{
+
+    public static function run($a, $b) {
+        return $a + $b;
+    }
+}
