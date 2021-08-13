1c968aae65f77d89f33a36881df1e7f620ea5177
Vitaliy <theyamshikov@yandex.ru>
2021-08-13 21:27:52 +0600

    upd

--- src/UserNotify/Domain/Interfaces/Services/TransportServiceInterface.php
+++ src/UserNotify/Domain/Interfaces/Services/TransportServiceInterface.php
@@ -3,10 +3,11 @@
 namespace ZnSandbox\Sandbox\UserNotify\Domain\Interfaces\Services;
 
 use ZnCore\Domain\Interfaces\Service\CrudServiceInterface;
+use ZnSandbox\Sandbox\UserNotify\Domain\Entities\NotifyEntity;
 
 interface TransportServiceInterface extends CrudServiceInterface
 {
 
-
+    public function send(NotifyEntity $notifyEntity);
 }
 
