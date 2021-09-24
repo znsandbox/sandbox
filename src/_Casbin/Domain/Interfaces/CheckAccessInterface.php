7edca3c906b0d9e678de1706bc4635121fdcc9f9
Vitaliy <theyamshikov@yandex.ru>
2021-09-24 12:53:58 +0600

    upd

--- src/_Casbin/Domain/Interfaces/CheckAccessInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ZnUser\Rbac\Domain\Interfaces;
-
-interface CheckAccessInterface
-{
-
-    public function checkAccess(?int $userId, string $permissionName, array $params = []);
-}
