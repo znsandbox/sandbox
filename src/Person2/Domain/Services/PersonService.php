c837be331d804d9c2fb34dd0f020c51746e44ed0
Vitaliy <theyamshikov@yandex.ru>
2021-12-15 20:51:17 +0600

    upd

--- src/Person2/Domain/Services/PersonService.php
+++ src/Person2/Domain/Services/PersonService.php
@@ -3,6 +3,7 @@
 namespace ZnSandbox\Sandbox\Person2\Domain\Services;
 
 use ZnCore\Domain\Base\BaseCrudService;
+use ZnCore\Domain\Helpers\EntityHelper;
 use ZnCore\Domain\Interfaces\Libs\EntityManagerInterface;
 use ZnSandbox\Sandbox\Person2\Domain\Entities\PersonEntity;
 use ZnSandbox\Sandbox\Person2\Domain\Interfaces\Repositories\PersonRepositoryInterface;
@@ -22,5 +23,12 @@ class PersonService extends BaseCrudService implements PersonServiceInterface
     public function getEntityClass(): string
     {
         return PersonEntity::class;
+    }
+
+    public function persist(object $entity)
+    {
+        $uniqueEntity = $this->getEntityManager()->oneByUnique($entity);
+        EntityHelper::setAttributesFromObject($uniqueEntity, $entity);
+        parent::persist($entity);
     }
 }
