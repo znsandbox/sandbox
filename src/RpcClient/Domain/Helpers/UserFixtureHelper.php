ec263ac06c82dd01ed3fc379bcd403fd34375ddb
ZnDeveloper <zndev@ya.ru>
2022-03-23 20:40:28 +0600

    upd

--- src/RpcClient/Domain/Helpers/UserFixtureHelper.php
+++ src/RpcClient/Domain/Helpers/UserFixtureHelper.php
@@ -7,13 +7,13 @@ use ZnBundle\User\Domain\Enums\CredentialTypeEnum;
 class UserFixtureHelper
 {
 
-    public static function generate($identityFixture, $credentialFixture): array
+    public static function generate($identityCollection, $credentialCollection): array
     {
-        $identityFixture = \ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper::index($identityFixture, 'id');
+        $identityCollection = \ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper::index($identityCollection, 'id');
         $collection = [];
-        foreach ($credentialFixture as $credential) {
-            if($credential['type'] == CredentialTypeEnum::LOGIN || $credential['type'] == CredentialTypeEnum::EMAIL) {
-                $identity = $identityFixture[$credential['identity_id']];
+        foreach ($credentialCollection as $credential) {
+            if (in_array($credential['type'], [CredentialTypeEnum::LOGIN, CredentialTypeEnum::EMAIL])) {
+                $identity = $identityCollection[$credential['identity_id']];
                 $collection[] = [
                     'login' => $identity['username'] ?? $credential['credential'],
                     'password' => 'Wwwqqq111',
