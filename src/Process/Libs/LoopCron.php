6681bdcea90b64dfff5165e7003d350b2d7413d2
Vitaliy <theyamshikov@yandex.ru>
2022-06-08 13:27:41 +0600

    upd

--- src/Process/Libs/LoopCron.php
+++ src/Process/Libs/LoopCron.php
@@ -5,10 +5,8 @@ namespace ZnSandbox\Sandbox\Process\Libs;
 use Symfony\Component\Lock\Exception\LockConflictedException;
 use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Lock\LockInterface;
-use Symfony\Component\Lock\Store\SemaphoreStore;
 use ZnCore\Base\Enums\Measure\TimeEnum;
 use ZnSandbox\Sandbox\Process\Exceptions\AlreadyStartedException;
-//use ZnSandbox\Sandbox\Process\Exceptions\LockedException;
 
 class LoopCron
 {
@@ -20,19 +18,20 @@ class LoopCron
     private $name = null;
     //private $locker = null;
     private $lock = null;
+    private $lockFactory;
 
-    public function __construct(string $name)
+    public function __construct(string $name, LockFactory $lockFactory)
     {
         $this->name = $name;
-        $this->lock = $this->createLockInstance($name);
-        //$this->locker = new LockProcess($name, $this->sleepIntervalMicrosecond);
+        $this->lockFactory = $lockFactory;
     }
 
-    protected function createLockInstance(string $name): LockInterface
+    protected function getLocker(): LockInterface
     {
-        $store = new SemaphoreStore();
-        $factory = new LockFactory($store);
-        return $factory->createLock($name, 30);
+        if (!$this->lock) {
+            $this->lock = $this->lockFactory->createLock($this->name);
+        }
+        return $this->lock;
     }
 
     public function setSleepIntervalMicrosecond($sleepIntervalMicrosecond): void
@@ -56,12 +55,9 @@ class LoopCron
             if ($this->isStarted) {
                 throw new AlreadyStartedException('Already started!');
             }
-            if (!$this->lock->acquire()) {
+            if (!$this->getLocker()->acquire()) {
                 throw new LockConflictedException('Locked in other process!');
             }
-            /*if ($this->locker->isLocked()) {
-                throw new LockedException('Locked in other process!');
-            }*/
         }
         $this->isStarted = $isStarted;
     }
@@ -73,15 +69,13 @@ class LoopCron
             return;
         }
         $this->setIsStarted(true);
-//        $this->locker->lock();
         $this->loop();
     }
 
     public function stop(): void
     {
         $this->setIsStarted(false);
-//        $this->locker->unlock();
-        $this->lock->release();
+        $this->getLocker()->release();
     }
 
     public function pause(): void
@@ -91,8 +85,7 @@ class LoopCron
 
     public function tick(): void
     {
-//        $this->locker->touch();
-        $this->lock->refresh();
+        $this->getLocker()->refresh();
     }
 
     protected function loop(): void
