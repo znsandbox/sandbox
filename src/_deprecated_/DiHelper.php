b270503f2688577ea7f8ca7f275d07685767a27f
ZnDeveloper <zndev@ya.ru>
2022-06-12 22:46:30 +0600

    upd

---
+++ src/_deprecated_/DiHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ZnCore\Base\Helpers;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface as SymfonyContainerAwareInterface;
+use ZnCore\Base\Libs\Container\Interfaces\ContainerAwareInterface;
+
+DeprecateHelper::hardThrow();
+
+class DiHelper
+{
+
+    /**
+     * Создать объект
+     *
+     * Пример: $widget = DiHelper::make($widgetClass, $this->container);
+     *
+     * @param string $className
+     * @param ContainerInterface $container
+     * @return object
+     */
+    public static function make(string $className, ContainerInterface $container): object
+    {
+        if ($container->has($className)) {
+            $instance = $container->get($className);
+        } else {
+            $instance = new $className;
+        }
+        self::setContainer($instance, $container);
+        return $instance;
+    }
+
+    public static function setContainer(object $instance, ContainerInterface $container)
+    {
+        if ($instance instanceof ContainerAwareInterface || $instance instanceof SymfonyContainerAwareInterface) {
+            $instance->setContainer($container);
+        }
+    }
+}
