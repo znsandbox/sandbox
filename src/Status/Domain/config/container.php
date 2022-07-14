c59a22299e38dd72c752537fee0861b89e568fb9
Vitaliy <theyamshikov@yandex.ru>
2022-07-14 12:21:11 +0600

    upd

--- src/Status/Domain/config/container.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'singletons' => [
-        //'ZnSandbox\Sandbox\Status\Domain\Interfaces\Services\StatusServiceInterface' => 'ZnSandbox\Sandbox\Status\Domain\Services\StatusService',
-
-        'ZnSandbox\Sandbox\Status\Domain\Interfaces\Repositories\StatusRepositoryInterface' => 'ZnSandbox\Sandbox\Status\Domain\Repositories\Enum\StatusRepository',
-    ],
-];
