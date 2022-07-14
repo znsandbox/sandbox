89a421019bbda9fa872afb151ecc7525620861d5
Vitaliy <theyamshikov@yandex.ru>
2022-07-14 11:42:56 +0600

    upd

--- src/Layout/Domain/config/container.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'singletons' => [
-        'ZnSandbox\Sandbox\Layout\Domain\Interfaces\Services\MenuServiceInterface' => 'ZnSandbox\Sandbox\Layout\Domain\Services\MenuService',
-        'ZnSandbox\Sandbox\Layout\Domain\Interfaces\Repositories\MenuRepositoryInterface' => 'ZnSandbox\Sandbox\Layout\Domain\Repositories\File\MenuRepository',
-    ],
-];
