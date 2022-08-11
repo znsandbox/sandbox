a61608e971baf7ff615415d734ee8b06b868fa85
Vitaliy <theyamshikov@yandex.ru>
2022-08-11 14:18:14 +0600

    upd

--- src/Deployer/Commands/DeployCommand.php
+++ src/Deployer/Commands/DeployCommand.php
@@ -3,6 +3,7 @@
 namespace ZnSandbox\Sandbox\Deployer\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use ZnCore\Instance\Helpers\InstanceHelper;
@@ -18,6 +19,18 @@ class DeployCommand extends Command
     protected static $defaultName = 'deployer:server:deploy';
     private $io;
 
+    protected function configure()
+    {
+        $this->addArgument('projectName', InputArgument::OPTIONAL);
+        /*$this->addOption(
+            'wrapped',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            '',
+            false
+        );*/
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         set_time_limit(0);
@@ -26,15 +39,25 @@ class DeployCommand extends Command
         $output->writeln(['<fg=white># Deployer. Deploy</>']);
 
         $deployProfiles = ConfigProcessor::get('deployProfiles');
-        $profiles = array_keys($deployProfiles);
-        $selectedProfile = $this->io->choiceQuestion('Select profile', $profiles);
-        $profileConfig = $deployProfiles[$selectedProfile];
+        
+        $projectName = $input->getArgument('projectName');
+        if(empty($projectName)) {
+            $profiles = array_keys($deployProfiles);
+            $projectName = $this->io->choiceQuestion('Select profile', $profiles);
+        }
+
+        $profileConfig = $deployProfiles[$projectName];
         $deployShell = $this->createShellInstance($profileConfig['handler']);
-        $deployShell->run($selectedProfile);
+        $deployShell->run($projectName);
 
         $this->io->success('Success!');
 
         return Command::SUCCESS;
+    }
+
+    private function getProfileName(): string
+    {
+        
     }
 
     private function createShellInstance($shellDefinition)
